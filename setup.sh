de271019f8c71661012e7258108625e4af179b44
Zac Wolfe <zacw@safisystems.com>
2018-07-20 23:11:39 -0700

    checkout specific commits before patching mma and mido

--- setup.sh
+++ setup.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 git clone https://github.com/infojunkie/mma.git mma_main
 cd mma_main
+git checkout faa3a983cedff06fd9622df8a1729ea0e71c7b41
 git apply ../mma.patch
 python mma.py -G
 cd ..
 git clone https://github.com/olemb/mido.git
 cd mido
+git checkout e87384d7e5d62de361a65ab6b1d5d62750475e84
 git apply ../mido.patch
 pip install .
 cd ..
